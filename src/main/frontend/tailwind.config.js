05748a369831eaa86d64e7914627557eeacc64ce
Sergio Asensio <sergio120604@gmail.com>
2024-12-20 18:42:42 +0100

    .

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../resources/templates/**/*.{html,js}"],
+  content: [
+    "../resources/templates/**/*.html", // Rutas a las plantillas Thymeleaf (.html)
+    "../resources/static/**/*.{js,css}", // Archivos estáticos, como JS y CSS
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}	
+}
